4d026544d44d7700149b88babe61296f9a6ed315
elonner <elilnnr5@gmail.com>
2023-06-17 18:53:40 -0700

    stubs up necessary directories

---
+++ index.js
@@ -0,0 +1,32 @@
+require('dotenv').config();
+require('./config/database');
+const express = require('express');
+const app = express();
+const PORT = 4000;
+
+const http = require('http').Server(app);
+const cors = require('cors');
+const socketIO = require('socket.io')(http, {
+  cors: {
+      origin: "http://localhost:3000"
+  }
+});
+
+app.use(cors());
+
+socketIO.on('connection', (socket) => {
+  console.log(`${socket.id} user just connected!`);
+  socket.on('disconnect', () => {
+    console.log('A user disconnected');
+  });
+});
+
+app.get('/api', (req, res) => {
+  res.json({
+    message: 'Hello world',
+  });
+});
+
+http.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
